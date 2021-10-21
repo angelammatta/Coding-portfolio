e6533786b9b9b15854f8268111c42cd7e02ca807
angelammatta <47487152+angelammatta@users.noreply.github.com>
2021-10-21 11:47:35 -0400

    Update recur.java

--- recur.java
+++ recur.java
@@ -1,21 +1,16 @@
 public class recur{
 
-/*
+//until it reaches zero, prints out the n-2 # 
 public static void recur1(int n){
-
-if(n>0){
-
-recur1(n-2);
-
-}
-
-System.out.print(n+ " ");
-
+    if(n>0){
+     recur1(n-2);
+   }
+     System.out.print(n+ " ");
 }
 
 
 
-
+//until it reaches zero, prints out n-2 stars, decreases by n-1 each time but then in for loop stop before n so n-2 amount of stars 
 public static void recur2(int n){
 
     if(n>0){
@@ -28,56 +23,30 @@ public static void recur2(int n){
     System.out.println();
 
     }
-    
-   
-    
     }
     
 
 
-*/
 
+//until a can be evenly divided by b, alter a,(a-b) or b(b-a) so at the end you get the greatest common factor 
 public static int gcf(int a, int b){
-
-
-
-
 
 if(a%b==0){
 
-  return b;
-
- }
-
+   return b;
+  }
 
  else{
    
-  if(a>b)
-   return gcf(a-b,b);
+  if(a>b){
+    return gcf(a-b,b);
+  }
+     
 return gcf(a,b-a);
 
  }
 
-
-
  }
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -85,19 +54,13 @@ public static void main(String[] args){
 
 
 
-//recur2(1);
+recur2(1);
 
 
 System.out.print(gcf(7,3));
 
 
 }
-
-
-
-
-
-
 
 
 }
