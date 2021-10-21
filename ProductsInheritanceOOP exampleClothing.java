b12aa2e02c2baeb5ef7f771c5ba246cce0da472a
angelammatta <47487152+angelammatta@users.noreply.github.com>
2021-10-21 11:22:21 -0400

    Update and rename Clothing.java to ProductsInheritanceOOP exampleClothing.java

--- ProductsInheritanceOOP exampleClothing.java
+++ ProductsInheritanceOOP exampleClothing.java
@@ -1,26 +1,29 @@
 public class Clothing extends Product{
 
+  //clothing brand and size
 private String brand;
 private String size;
 
-
+    
+//populated constructor
 public Clothing(double p,String c,String n,int q,String b,String s){
 
 super(p,c,n,q);
 brand = b;
 size = s; 
 
-    
 }
 
 
 
+    //find the brand or the size
 public String getBrand(){
 
 return brand;
 
 }
 
+    
 public String getSize(){
 
     return size;
@@ -28,7 +31,7 @@ public String getSize(){
 
 
 
-
+//set the brand or size to a new value 
 public void setBrand(String b){
 
 brand = b;
@@ -48,28 +51,6 @@ public String toString(){
 return super.toString() + " \n Brand: "+ brand+ " \n Size: "+ size;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
